81496afc612c321e16e03b2b6ee991a55eea5abe
Joshua <Joshua@Joshua-PC>
2015-10-05 18:30:51 +0100

    Initial LibGDX Commit

---
+++ desktop/src/com/pokedroid/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pokedroid.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pokedroid.PokeDroid;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PokeDroid(), config);
+	}
+}
